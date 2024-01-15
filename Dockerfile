4deb3179f9341eb35495a9a33ca409e572eceec3
Vielman Paredes <pevjose@gmail.com>
2024-01-14 23:30:34 -0400

    fix

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ RUN pip install -r requirements.txt
 
 COPY ./ ./
 
-CMD [ "python", "manage.py", "runserver", "0.0.0.0:3000" ]
+CMD [ "python", "manage.py", "runserver", "0.0.0.0:8000" ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,10 +2,16 @@ from rest_framework import generics, authentication, permissions, status
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import pandas as pd
 
 class DashboardView(APIView):
     # authentication_classes = [authentication.TokenAuthentication]
     # permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, format=None):
-        return Response(status = status.HTTP_200_OK)
+
+        df = pd.read_csv('DataRedesSociales.csv', usecols=['text'])
+        df.head()
+        df.iloc[0].values
+        
+        return Response(df.iloc[0].values,status = status.HTTP_200_OK)
